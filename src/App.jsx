0cec7788b50672d8936d4f8e30e7253812b8f389
Diem Ly <diemrosely@gmail.com>
2024-07-14 13:15:53 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import NavBar from "./components/Navbar"; 
+import Hero from "./components/Hero"; 
+import Highlights from "./components/Highlights"; 
+import Model from "./components/Model"
+import Features from "./components/Features"
+
+
+const  App = () => {
+  return (
+    <main className='bg-black'>
+      <NavBar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+    </main>
+  )
+}
+
+export default App
